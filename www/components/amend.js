c3500bb8cef35b6aaba96ba35907dddcfe99dcac
Bernardo112 <bernardo.cruz@etec.sp.gov.br>
2023-03-08 03:09:37 +0900

    Project's Initial Commit

---
+++ www/components/amend.js
@@ -0,0 +1,5 @@
+function IMC(){
+    resultado = document.getElementById('KG').value / document.getElementById('altura').value * document.getElementById('altura').value;
+
+    document.getElementById('resultado').innerHTML = "Seu imc é: " + consumo;
+}
